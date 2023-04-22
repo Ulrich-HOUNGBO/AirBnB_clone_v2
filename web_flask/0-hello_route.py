75a1262385b8b2be13162beb3442b337dfc5e7da
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-04-22 16:43:50 +0100

    start Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():  # put application's code here
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
