40e5930f08919259efdfb518bb496926d00a31f0
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-04-22 19:04:30 +0100

    display HTML page only if n is an integer: H1 tag: “Number: n” inside the tag BODY

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -56,5 +56,6 @@ def html_in_int(n):
     """display HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
