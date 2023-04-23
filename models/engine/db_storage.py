50df9642aef49fa424791f3cc4d2fc3b502d2706
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-04-23 13:23:01 +0100

    start Flask web application

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
     Define class DatabaseStorage
-'''
+"""
 from os import getenv
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -12,16 +12,16 @@ from models.base_model import Base
 
 
 class DBStorage:
-    '''
+    """
         Create SQLalchemy database
-    '''
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        '''
+        """
             Create engine and link to MySQL databse (hbnb_dev, hbnb_dev_db)
-        '''
+        """
         user = getenv("HBNB_MYSQL_USER")
         pwd = getenv("HBNB_MYSQL_PWD")
         host = getenv("HBNB_MYSQL_HOST")
@@ -33,9 +33,9 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        '''
+        """
             Query current database session
-        '''
+        """
         db_dict = {}
 
         if cls != "":
@@ -56,35 +56,35 @@ class DBStorage:
             return db_dict
 
     def new(self, obj):
-        '''
+        """
             Add object to current database session
-        '''
+        """
         self.__session.add(obj)
 
     def save(self):
-        '''
+        """
             Commit all changes of current database session
-        '''
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        '''
+        """
             Delete from current database session
-        '''
+        """
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        '''
+        """
             Commit all changes of current database session
-        '''
+        """
         self.__session = Base.metadata.create_all(self.__engine)
         factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(factory)
         self.__session = Session()
 
     def close(self):
-        '''
+        """
             Remove private session attribute
-        '''
+        """
         self.__session.close()
