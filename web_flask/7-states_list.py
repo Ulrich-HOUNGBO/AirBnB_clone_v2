2d4ec168d8982d4153e7cb19a7a4d62e8064c8f4
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-04-23 22:03:18 +0100

    list of city

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,23 +2,18 @@
 """
 script starts Flask web app
     listen on 0.0.0.0, port 5000
-    routes: /:              display "Hello HBNB!"
-            /hbnb:          display "HBNB"
-            /c/<text>:      display "C" + text (replace underscores with space)
-            /python/<text>: display "Python" + text (default is "is cool")
-            /number/<n>:    display "n is a number" only if n is an integer
-            /number_template/<n>: display HTML page only if n is an integer
-            /number_odd_or_even/<n>: display HTML page only if n is an integer
-            /states_list: display a HTML page: (inside the tag BODY)
-                H1 tag: “States”
-                UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
-                LI tag: description of one State: <state.id>: <B><state.name></B>
+    routes: /:                    display "Hello HBNB!"
+            /hbnb:                display "HBNB"
+            /c/<text>:            display "C" + text (replace "_" with " ")
+            /python/<text>:       display "Python" + text (default="is cool")
+            /number/<n>:          display "n is a number" only if int
+            /number_template/<n>: display HTML page only if n is int
+            /number_odd_or_even/<n>: display HTML page; display odd/even info
+            /states_list:         display HTML and state info from storage;
 """
-
-from flask import Flask, render_template
-from models import *
 from models import storage
-
+from models import *
+from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -53,36 +48,44 @@ def python_text(text="is cool"):
 
 
 @app.route('/number/<int:n>')
-def text_in_int(n):
-    """display text only if n is an integer"""
-    return "{} is a number".format(n)
+def text_if_int(n):
+    """display text only if int given"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>')
-def html_in_int(n):
-    """display HTML page only if n is an integer"""
+def html_if_int(n):
+    """display html page only if int given
+       place given int into html template
+    """
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>')
 def html_odd_or_even(n):
-    """display HTML page only if n is an integer"""
+    """display html page only if int given
+       place given int into html template
+       substitute text to display if int is odd or even
+    """
     odd_or_even = "even" if (n % 2 == 0) else "odd"
     return render_template('6-number_odd_or_even.html',
                            n=n, odd_or_even=odd_or_even)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def tear_down(self):
+    """after each request remove current SQLAlchemy session"""
     storage.close()
 
 
 @app.route('/states_list')
-def states_list():
+def html_fetch_states():
     """display html page
-       fetch sorted states to insert into html in UL tag"""
-    states = list(storage.all("State").values())
-    return render_template('7-states_list.html', states=states)
+       fetch sorted states to insert into html in UL tag
+    """
+    state_objs = [s for s in storage.all("State").values()]
+    return render_template('7-states_list.html',
+                           state_objs=state_objs)
 
 
 if __name__ == "__main__":
