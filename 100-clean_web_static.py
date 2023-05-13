6d5221f88c1a86bcd89dbe88583647fedc545ebe
Ulrich houngbo <76060199+Ulrich-HOUNGBO@users.noreply.github.com>
2023-05-13 02:02:45 +0100

    Update 100-clean_web_static.py
    fixe server ip

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,7 +11,7 @@ Usage:
 from fabric.api import local, env, put, run
 from time import strftime
 import os.path
-env.hosts = ['35.229.54.225', '35.231.225.251']
+env.hosts = ['54.146.9.212', '100.25.141.239']
 
 
 def do_pack():
